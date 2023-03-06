f5728fbe1ab04e40c6ff3d36be1e1036e59e1850
yanxu <yyxx20150909@163.com>
2023-03-06 10:53:49 +0800

    Modify logs

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ import markdown.extensions.fenced_code
 import markdown.extensions.codehilite
 import tiktoken
 from MessageQueue import MessageQueue
+from datetime import datetime
+
 
 model_name = "gpt-3.5-turbo"
 encoding = tiktoken.encoding_for_model(model_name)
-openai.api_key = 'xxx'
+openai.api_key = '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'
 app = Flask(__name__)
 messages = {}
 
@@ -26,7 +28,10 @@ def get_bot_response():
     else:
         ip = request.remote_addr
     user_input = request.form['user_input']
-    log_info = "{} - - Message: {}".format(ip, user_input)
+    now = datetime.now()
+    year_month_day = now.strftime("%Y-%m-%d")
+    log_info = "{} - {} - Message: {}".format(ip, year_month_day, user_input)
+    print(log_info, flush=True)
     with open("logs/debug.log", 'a+', encoding="utf8") as file:
         file.write(str(log_info) + '\n')
     if ip in messages.keys():
@@ -41,7 +46,6 @@ def get_bot_response():
     else:
         try:
             message_request = messages_queue.get_recent_messages()
-            print(message_request)
             completion = openai.ChatCompletion.create(model=model_name,
                                                     messages=message_request,
                                                     timeout=5)
@@ -50,7 +54,6 @@ def get_bot_response():
             final_response = api_response
         except Exception as err:
             final_response = "调用失败，原因为：" + str(err) + "\n 如果为网络原因请稍后再试，梯子可能不稳定；如果是token超过上限，请点击清空按钮即可。"
-    print("{} - - Message: {}".format(ip, final_response))
     return Markup(markdown.markdown(final_response,extensions=['fenced_code', 'codehilite']))
 
 
@@ -61,6 +64,11 @@ def reset():
         ip = request.headers.getlist("X-Forwarded-For")[0]
     else:
         ip = request.remote_addr
+    now = datetime.now()
+    year_month_day = now.strftime("%Y-%m-%d")
+    log_info = "{} - {} - Message: Reset context.".format(ip, year_month_day)
+    with open("logs/debug.log", 'a+', encoding="utf8") as file:
+        file.write(str(log_info) + '\n')
     global messages
     if ip in messages.keys():
         del messages[ip]
