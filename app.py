4778c9000ea2c39a9a9481584c67feffdc8d24a2
yanxu <yyxx20150909@163.com>
2023-03-27 09:21:50 +0800

    muture version

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from datetime import datetime
 
 model_name = "gpt-3.5-turbo"
 encoding = tiktoken.encoding_for_model(model_name)
-openai.api_key = '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'
+openai.api_key = 'sk-xxx'
 app = Flask(__name__)
 messages = {}
 
@@ -29,11 +29,9 @@ def get_bot_response():
         ip = request.remote_addr
     user_input = request.form['user_input']
     now = datetime.now()
-    year_month_day = now.strftime("%Y-%m-%d")
-    log_info = "{} - {} - Message: {}".format(ip, year_month_day, user_input)
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    log_info = "{} - {} - Message: {}".format(ip, current_time, user_input)
     print(log_info, flush=True)
-    with open("logs/debug.log", 'a+', encoding="utf8") as file:
-        file.write(str(log_info) + '\n')
     if ip in messages.keys():
         messages_queue = messages[ip]
     else:
@@ -48,12 +46,16 @@ def get_bot_response():
             message_request = messages_queue.get_recent_messages()
             completion = openai.ChatCompletion.create(model=model_name,
                                                     messages=message_request,
-                                                    timeout=5)
+                                                    timeout=5,
+                                                    request_timeout=15)
             api_response = completion.choices[0].message['content']
             messages_queue.add_message(api_response, role=MessageQueue.ASSISTANT)
             final_response = api_response
         except Exception as err:
             final_response = "调用失败，原因为：" + str(err) + "\n 如果为网络原因请稍后再试，梯子可能不稳定；如果是token超过上限，请点击清空按钮即可。"
+    with open("logs/debug.log", 'a+', encoding="utf8") as file:
+        file.write(str(log_info) + '\n')
+        file.write(str(final_response) + '\n')
     return Markup(markdown.markdown(final_response,extensions=['fenced_code', 'codehilite']))
 
 
@@ -65,8 +67,8 @@ def reset():
     else:
         ip = request.remote_addr
     now = datetime.now()
-    year_month_day = now.strftime("%Y-%m-%d")
-    log_info = "{} - {} - Message: Reset context.".format(ip, year_month_day)
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    log_info = "{} - {} - Message: Reset context.".format(ip, current_time)
     with open("logs/debug.log", 'a+', encoding="utf8") as file:
         file.write(str(log_info) + '\n')
     global messages
